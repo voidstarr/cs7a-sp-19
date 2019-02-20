6fcd5265b7198ea6bc9ebcc56db4488c24bfa790
voidstarr <voidstarr@users.noreply.github.com>
2019-02-19 18:15:20 -0800

    initial commit

---
+++ lab-3/01ThousandSeperator/01ThousandSeperator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <locale>
+
+using namespace std;
+
+int main() {
+  int num;
+  
+  cout << "Program Part 1 starts here" 
+       << endl 
+       << endl
+       << "Enter a number between 1 - 999999: ";
+  
+  cin >> num;
+
+  cout.imbue(locale(""));
+
+  cout << fixed << "The number is: " << num
+       << endl
+       << endl;
+  
+  cin.ignore();
+
+  string numString;
+  cout << "Program Part 2 starts here"
+       << endl
+       << endl
+       << "Enter a number between 1 - 999999: ";
+
+  getline(cin, numString);
+
+  if (numString.length() > 3) {
+    numString.insert(numString.length()-3,",");
+  }
+
+  cout << "The number is: "
+       << numString << endl;
+}
